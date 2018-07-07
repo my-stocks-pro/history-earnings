8dd12da9d18d4e4d8058d26122c9d73734a83582
alex <sklif.85@gmail.com>
2018-07-07 11:36:39 +0300

    get add

--- classEarnings.py
+++ classEarnings.py
@@ -1,5 +1,9 @@
 from classRequest import Requester
 import json
+import datetime
+from dateutil.relativedelta import relativedelta
+from dateutil.rrule import rrule, MONTHLY
+import pandas as pd
 
 
 class Earnings(Requester):
@@ -13,27 +17,34 @@ class Earnings(Requester):
             "https://submit.shutterstock.com/earnings/daily?page={}&date={}&language=en&category=enhanced&sort=desc&sorted_by=count&per_page=20",
             "https://submit.shutterstock.com/earnings/daily?page={}&date={}&language=en&category=single_image_and_other&sort=desc&sorted_by=count&per_page=20"]
 
-    def get(self):
-        # print(query_string)
-        # date_range = json.loads(query_string.decode('utf-8'))
-        print(self.start)
-        print(self.end)
+    def date_list(self):
+        return [dt for dt in rrule(MONTHLY, dtstart=self.start, until=self.end)]
+
+    def date_range(self):
+        dl = self.date_list()
+        for date in dl:
+            self.get_by_dete(date)
+
+    def get_by_dete(self, date):
         pass
-        # for url in self.urls:
-        #     page = 1
-        #     while True:
-        #         tmp_url = url.format(str(page), str(date_current))
-        #         try:
-        #             r = request.get_response(tmp_url)
-        #         except():
-        #             self.to_logger("error in request")
-        #         if int(r.url[r.url.index("=") + 1:r.url.index("&")]) < page:
-        #             self.to_logger("empty url ->" + tmp_url)
-        #             break
-        #         self.to_logger(tmp_url)
-        #         page += 1
-        #         try:
-        #             df = pd.read_html(r.content)
-        #         except ValueError:
-        #             print(ValueError)
-        #             break
+
+    def get(self):
+        print(self.date_range())
+        for url in self.urls:
+            page = 1
+            while True:
+                tmp_url = url.format(str(page), str(date_current))
+                try:
+                    r = self.get_response(tmp_url)
+                except():
+                    self.to_logger("error in request")
+                if int(r.url[r.url.index("=") + 1:r.url.index("&")]) < page:
+                    self.to_logger("empty url ->" + tmp_url)
+                    break
+                self.to_logger(tmp_url)
+                page += 1
+                try:
+                    df = pd.read_html(r.content)
+                except ValueError:
+                    print(ValueError)
+                    break
--- earnings-history-service.py
+++ earnings-history-service.py
@@ -2,25 +2,28 @@ from flask import Flask, render_template, request, flash
 from flask_cors import CORS
 from classEarnings import Earnings
 
-app = Flask(__name__)
-# app.debug = True
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 
-@app.route("/history/earnings", methods=['GET'])
+
+# app = Flask(__name__)
+# cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+#
+#
+# @app.route("/history/earnings", methods=['GET'])
 def get_data():
-    query = request.args
-    earnings.start = query.get('start')
-    earnings.end = query.get('end')
+    # query = request.args
+    # earnings.start = query.get('start')
+    # earnings.end = query.get('end')
+    earnings.start = pd.to_datetime("2018-05-01")
+    earnings.end = pd.to_datetime("2018-06-01")
     earnings.get()
     return ""
 
-# @server.route("/history/earnings", methods=['POS'])
-# def range():
-#     return
+
 
 
 if __name__ == '__main__':
     earnings = Earnings()
-    app.run(host='127.0.0.1', port=8003)
-
+    # test()
+    get_data()
+    # app.run(host='127.0.0.1', port=8003)
